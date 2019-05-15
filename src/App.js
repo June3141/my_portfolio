a5901b0a534dbb270ca1a34add2578fb62a15295
jungoya@realglobe.jp <ryoushi0620@gmail.com>
2019-05-15 18:14:00 +0900

    構成を変更

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Header from './containers/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const NotFound = () => {
+  return(
+    <h2> ページが見つかりません． </h2>
+  )
+}
+
+class App extends Component{
+  render() {
+    return (
+      <div className="App">
+       <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
