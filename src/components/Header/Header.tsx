00d844e9e8b009ec8711853029aa3c5599520e45
june3141 <ryoushi0620@gmail.com>
2020-02-10 11:01:01 +0900

    Header の内容とcssを修正

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,9 +6,7 @@ export const Header = () => {
     return (
         <div className="Header">
             <header className="Header-display">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
+                <p>Jun's Portfolio.</p>
             </header>
         </div>
     );
