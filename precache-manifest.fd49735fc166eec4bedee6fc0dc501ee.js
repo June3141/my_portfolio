df3db3ca4ddcc52c0e2139772bc0b607e6e62ddf
june3141 <ryoushi0620@gmail.com>
2020-02-10 11:08:04 +0900

    Updates

---
+++ precache-manifest.fd49735fc166eec4bedee6fc0dc501ee.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34645d6ab426bd415752000ba3ff4adf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7e8e0dd41532d47d7838",
+    "url": "./static/css/main.8f2afc79.chunk.css"
+  },
+  {
+    "revision": "a50d3af18db54d4610ca",
+    "url": "./static/js/2.88e8dc4d.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "./static/js/2.88e8dc4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e8e0dd41532d47d7838",
+    "url": "./static/js/main.315de77d.chunk.js"
+  },
+  {
+    "revision": "3cffdea7db2cc37140c8",
+    "url": "./static/js/runtime-main.692de215.js"
+  },
+  {
+    "revision": "596be931c446750079012378f9d695bc",
+    "url": "./static/media/Fortran.596be931.svg"
+  },
+  {
+    "revision": "e7c6c73f71f6fd52151cfc2d65006f29",
+    "url": "./static/media/git.e7c6c73f.svg"
+  },
+  {
+    "revision": "5903a1df2f4b6dc96e4db491de5fbea7",
+    "url": "./static/media/github-octocat.5903a1df.svg"
+  },
+  {
+    "revision": "266d1cb060adfe97e0436047b746c9f4",
+    "url": "./static/media/gopher.266d1cb0.svg"
+  },
+  {
+    "revision": "89e1cae5e53197ebf2751139846b7487",
+    "url": "./static/media/haskell.89e1cae5.svg"
+  },
+  {
+    "revision": "24845c5ca600cfab149e84387d826465",
+    "url": "./static/media/html-5.24845c5c.svg"
+  },
+  {
+    "revision": "87711407412494d529d45ee76167583c",
+    "url": "./static/media/javascript.87711407.svg"
+  },
+  {
+    "revision": "733a8e9dbf7dd69c2f7f46213e2d9541",
+    "url": "./static/media/lifeGamePython3.733a8e9d.webm"
+  },
+  {
+    "revision": "061fac639a1446d09785658534318258",
+    "url": "./static/media/profile_icon.061fac63.png"
+  },
+  {
+    "revision": "51c2eab22c8994e70e7633979982b451",
+    "url": "./static/media/python.51c2eab2.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my_portfolio/precache-manifest.ed96fb05fdfdeddad20e2a1dbec1685c.js"
+  "./precache-manifest.fd49735fc166eec4bedee6fc0dc501ee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my_portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
